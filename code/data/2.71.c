5b95635859c0c9114d1c6e6cf0d71856b4dadd8e
duncan <duncand9199@gmail.com>
2025-12-18 21:05:50 -0500

    Started 2.71.c

---
+++ code/data/2.71.c
@@ -0,0 +1,35 @@
+/*
+ * You just started working for a company that is implementing
+ * a set of procedures to operate on a data structure where 4 signed 
+ * bytes are packed into a 32-bit unsigned. Bytes within the word are 
+ * numbered from 0 (least significant) to 3 (most significant). You 
+ * have been assigned the task of implementing a function for a 
+ * machine using two's complement arithmetic and arithmetic right shifts 
+ * for the following prototype:
+ *
+ * Declaration of data type where 4 bytes are packed into an unsigned
+ * typedef unsigned packed_t;
+ *
+ * Extract byte from word. Return as signed integer
+ * int xbyte(packed_t word, int bytenum);
+ *
+ * That is, the function will extract the designated byte and sign 
+ * extedn it to be a 32-bit int.
+ *
+ * Your predecessor (who was fired for incompetence) wrote the following
+ * code:
+ *
+ * int xbyte(packed_t word, int bytenum) {
+ *    return (word >> (bytenum << 3)) & 0x0FF;
+ * }
+ *
+ * A. What is wrong with this code?
+ * B. Give a correct implementation of the function that uses only left 
+ * and right shifts, along with one subtraction.
+*/
+
+typedef unsigned packed_t;
+
+int xbyte(packed_t word, int bytenum) {
+	return (word >> (bytenum << 3)) & 0x0FF;
+}
